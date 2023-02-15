728f625328ea8f3fc25ade7a943d67c3026c7da2
hudson203 <hudsonnum0@gmail.com>
2023-02-15 19:41:26 +0900

    add INS_LDA_IM

--- main/main_6502.cpp
+++ main/main_6502.cpp
@@ -1,3 +1,14 @@
+/*
+ * File: main_6502.cpp
+ * Project: main
+ * File Created: Monday, 13th February 2023 3:20:48 pm
+ * Author: Shinyoung Kim(hudson203) (hudsonnum0@gmail.com)
+ * -----
+ * Last Modified: Wednesday, 15th February 2023 3:27:23 pm
+ * Modified By: Shinyoung Kim(hudson203) (hudsonnum0@gmail.com>)
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -5,12 +16,11 @@
 
 using Byte = unsigned char;
 using Word = unsigned short;
-
 using u32 = unsigned int;
 
 struct Mem
 {
-    static constexpr u32 MAX_MEM = 1024 *64;
+    static constexpr u32 MAX_MEM = 1024 * 64;
     Byte Data[MAX_MEM];
 
     void Initialise()
@@ -21,13 +31,19 @@ struct Mem
         }
     }
 
-    /*read 1 byte*/
+    /**read 1 byte*/
     Byte operator[]( u32 Address ) const
     {
-        // assert here Address is < MAx_MEM
+        // assert here Address is < MAX_MEM
         return Data[Address];
     }
 
+    /**write 1 byte*/
+    Byte& operator[]( u32 Address ) 
+    {
+        // assert here Address is < MAX_MEM
+        return Data[Address];
+    }
 };
 
 struct CPU
@@ -50,7 +66,7 @@ struct CPU
     {
         PC = 0xFFFC;
         SP = 0x0100;
-        C= Z = I= D = B = V = N = 0;
+        C = Z = I = D = B = V = N = 0;
         A = X = Y = 0;
         memory.Initialise();
     }
@@ -63,12 +79,29 @@ struct CPU
         return Data;
     }
 
+    // opcodes
+    static constexpr Byte
+        INS_LDA_IM = 0xA9;
+
     void Execute( u32 Cycles, Mem& memory )
     {
-        while ( Cycles >0)
+        while ( Cycles > 0 )
         {
             Byte Ins = FetchByte( Cycles, memory );
-            (void)Ins;
+            switch ( Ins )
+            {
+            case INS_LDA_IM:
+            {
+                Byte Value = FetchByte( Cycles, memory );
+                A = Value;
+                Z = (A == 0);
+                N = (A & 0b10000000) > 0;
+            } break;
+            default:
+            {
+                printf("Instruction not handled %d", Ins);
+            } break;
+            }
         }
     }
 };
@@ -78,6 +111,10 @@ int main()
     Mem mem;
     CPU cpu;
     cpu.Reset( mem );
+    // start - inline a little program
+    mem[0xFFFC] = CPU::INS_LDA_IM;
+    mem[0xFFFD] = 0x42;
+    // end - inline a little program
     cpu.Execute( 2, mem );
     return 0;
 }
